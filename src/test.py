55b842e36e8f4c99e14ed319c9d4786ea1511ae6
A Lee <alee@fwparker.org>
2019-12-26 20:39:56 -0600

    Update test.py

--- src/test.py
+++ src/test.py
@@ -37,7 +37,7 @@ while not done:
  
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(WHITE)
+    screen.fill(GREEN)
  
     # --- Drawing code should go here
  
